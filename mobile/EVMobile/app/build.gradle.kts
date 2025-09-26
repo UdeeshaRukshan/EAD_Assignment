d6cec5b0c8c8bf73a142bcea464d19043136454e
UdeeshaRukshan <IT21807480@my.sliit.lk>
2025-09-26 13:03:32 +0530

    latest changes

--- mobile/EVMobile/app/build.gradle.kts
+++ mobile/EVMobile/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.kapt)
-    alias(libs.plugins.kotlin.parcelize)
+    kotlin("kapt")
+    id("kotlin-parcelize")
 }
 
 android {
