ee4eb8a006dc345b8501eafdcfce2a6a52c6091b
Nadu_Dev <inbox.nadun@gmail.com>
2025-10-10 10:53:12 +0530

    chore: Add missing file header comments to ErrorViewModel.cs

--- src/EVChargingStation.Web/Models/ErrorViewModel.cs
+++ src/EVChargingStation.Web/Models/ErrorViewModel.cs
@@ -1,3 +1,10 @@
+/*
+ * File: ErrorViewModel.cs
+ * Purpose: Represents error information for the EV Charging Station Web application
+ * Author: EAD Assignment
+ * Date: 2025
+ */
+
 namespace EVChargingStation.Web.Models;
 
 public class ErrorViewModel
